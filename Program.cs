2caaac8ac15aeb073db68afbf8325d5380edc261
Jana Bojarova <thejana13@gmail.com>
2022-04-05 20:07:28 +0300

    first commkit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoopSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Random rnd = new Random();
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)     
+            {
+                int myRandom = rnd.Next(1, 11);
+                sum = sum + myRandom;
+                
+
+            }
+            Console.WriteLine(sum);
+         
+
+
+            
+
+
+           
+
+        }
+    }
+}
